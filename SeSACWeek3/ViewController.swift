f35ba082447e1a6a1c2b85ecc72128c7e34303af
moklad <dev.moklad@gmail.com>
2025-07-11 11:10:37 +0900

    Initial Commit

---
+++ SeSACWeek3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SeSACWeek3
+//
+//  Created by Youngjun Kim on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(1)
+        print(2)
+        print(3)
+        print(4)
+        print(5)
+    }
+
+
+}
+
