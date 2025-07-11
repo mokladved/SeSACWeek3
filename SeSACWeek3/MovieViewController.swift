95294238d3655d358afaf84239f229f4b762ab22
moklad <dev.moklad@gmail.com>
2025-07-11 12:13:02 +0900

    MovieViewController 파일 생성

---
+++ SeSACWeek3/MovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewController.swift
+//  SeSACWeek3
+//
+//  Created by Youngjun Kim on 7/11/25.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
